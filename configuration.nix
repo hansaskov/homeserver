fd0d07cfc7eb4d72cbc6095b9af64143becea22e
Hans Askov <hansaskov1999@gmail.com>
2025-06-29 13:14:00 +0200

    simplify configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,8 @@
   # System Configuration
   system.stateVersion = "23.11";
   nixpkgs.config.allowUnfree = true;
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
 
   # Boot Configuration
   boot.loader.grub = {
@@ -39,21 +41,6 @@
 
   # Localization
   time.timeZone = "Europe/Copenhagen";
-  i18n = {
-    defaultLocale = "en_GB.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "da_DK.UTF-8";
-      LC_IDENTIFICATION = "da_DK.UTF-8";
-      LC_MEASUREMENT = "da_DK.UTF-8";
-      LC_MONETARY = "da_DK.UTF-8";
-      LC_NAME = "da_DK.UTF-8";
-      LC_NUMERIC = "da_DK.UTF-8";
-      LC_PAPER = "da_DK.UTF-8";
-      LC_TELEPHONE = "da_DK.UTF-8";
-      LC_TIME = "da_DK.UTF-8";
-    };
-  };
-  console.keyMap = "dk-latin1";
 
   # User Management
   users.users.homeserver = {
@@ -65,11 +52,8 @@
 
   # System Packages
   environment.systemPackages = with pkgs; [
-    wget
     git
-    bash
     docker
-    docker-compose
   ];
 
   # System Services
